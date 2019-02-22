9903bc127008626e386407fc49f0a6218e5495b1
Andrew Fulton <afulton20@gmail.com>
2019-02-22 11:20:22 -0600

    Detect a focus area
    * the focus area is incorrect and needs some adjusting

--- rangler.py
+++ rangler.py
@@ -1,10 +1,15 @@
 import cv2 as cv
 import numpy as np
 
-img_path = '/Users/andrewfulton/Documents/School/Research/rangle/Root angle images/Bread Wheat/Images/IMG_7103.JPG'
-
+img_path = '/Users/andrewfulton/Documents/School/Research/rangle/Root angle images/Bread Wheat/Images/IMG_7104.JPG'
 
 # np.set_printoptions(threshold=np.nan)
+
+
+def onMouseClicked(event, x, y, flags, img_param):
+    if event == cv.EVENT_LBUTTONUP:
+        print(img_param[y, x])
+
 
 def show_image(name, img, w, h):
     screen_res = 1280.0, 720.0
@@ -19,6 +24,7 @@ def show_image(name, img, w, h):
     cv.namedWindow(name, cv.WINDOW_NORMAL)
     cv.resizeWindow(name, window_width, window_height)
     cv.imshow(name, img_resized)
+    cv.setMouseCallback(name, onMouseClicked, img_resized)
 
 
 def show_images(images, w, h):
@@ -43,24 +49,66 @@ h, w = img.shape[:2]
 
 # get masks for planting locations
 hue_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-lower_hue_range = cv.inRange(hue_img, np.array([0, 40, 150]), np.array([20, 90, 230]))
-upper_hue_range = cv.inRange(hue_img, np.array([20, 90, 230]), np.array([80, 255, 255]))
+lower_hue_range = cv.inRange(hue_img, np.array([10, 40, 140]), np.array([30, 70, 210]))
 p_loc_mask = cv.addWeighted(lower_hue_range, 1.0, 1, 1.0, 0.0)
 
 # Apply mask to black + white image
 grey_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 p_loc_img = cv.bitwise_and(p_loc_mask, grey_img)
-thresh, p_loc_bw_img = cv.threshold(p_loc_img, 30, 255, cv.THRESH_BINARY)
+thresh, p_loc_bw_img = cv.threshold(p_loc_img, 130, 255, cv.THRESH_BINARY)
+
+blobDetectorParams = cv.SimpleBlobDetector_Params()
+blobDetectorParams.blobColor = 255
+blobDetectorParams.filterByConvexity = False
+
+blobDetector = cv.SimpleBlobDetector_create(blobDetectorParams)
+
+keypoints = blobDetector.detect(p_loc_bw_img)
+
+points = np.asarray([kp.pt for kp in keypoints])
+
+x_values = points[:, 0]
+y_values = points[:, 1]
+
+left_x = min(x_values)
+right_x = max(x_values)
+mid_x = left_x + (right_x - left_x) / 2.0
+
+min_dist = -1
+mid_key_index = 0
+
+for i, kp in enumerate(keypoints):
+    x = kp.pt[0]
+    dist = abs(x - mid_x)
+    if dist < min_dist or min_dist < 0:
+        min_dist = dist
+        mid_key_index = i
+
+
+focus_x = int(x_values[mid_key_index])
+focus_y = int(y_values[mid_key_index])
+half_focus_area_width = 300 / 2
+half_focus_area_height = 500 / 2
+
+print(focus_x, focus_y)
+
+p_loc_img_color = cv.cvtColor(p_loc_img, cv.COLOR_GRAY2BGR)
+focus_img = cv.rectangle(
+    p_loc_img_color,
+    (focus_x - half_focus_area_width, focus_y + half_focus_area_width),
+    (focus_x + half_focus_area_width, focus_y - half_focus_area_height),
+    (0, 255, 0),
+    thickness=6)
 
 show_images([
+    ('focus_rec', focus_img),
+    ('p_loc_bw', p_loc_bw_img),
+    ('p_loc', p_loc_img),
     ('original', img),
     ('hue_img', hue_img),
     ('lower_hue_range', lower_hue_range),
     ('lower_mask', p_loc_mask),
-    ('upper_hue_range', upper_hue_range),
     ('grey', grey_img),
-    ('p_loc', p_loc_img),
-    ('p_loc_bw', thresh),
 ], w, h)
 
 cv.destroyAllWindows()
