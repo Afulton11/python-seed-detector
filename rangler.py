a009af5a6e898d42cdbc6b77e60ce263cc4f4507
Andrew Fulton <afulton20@gmail.com>
2019-02-25 09:35:04 -0600

    Refactor variable names and structure

--- rangler.py
+++ rangler.py
@@ -6,6 +6,76 @@ img_path = '/Users/andrewfulton/Documents/School/Research/rangle/Root angle imag
 # np.set_printoptions(threshold=np.nan)
 
 
+def run():
+    img = cv.imread(img_path, cv.IMREAD_COLOR)
+    h, w = img.shape[:2]
+
+    # get masks for planting locations
+    hue_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    lower_hue_range = cv.inRange(hue_img, np.array([10, 40, 140]), np.array([30, 70, 210]))
+    p_loc_mask = cv.addWeighted(lower_hue_range, 1.0, 1, 1.0, 0.0)
+
+    # Apply mask to black + white image
+    grey_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    p_loc_img = cv.bitwise_and(p_loc_mask, grey_img)
+    thresh, p_loc_bw_img = cv.threshold(p_loc_img, 10, 255, cv.THRESH_BINARY)
+
+    blob_params = cv.SimpleBlobDetector_Params()
+    blob_params.blobColor = 255
+    blob_params.filterByConvexity = False
+
+    blob_detector = cv.SimpleBlobDetector_create(blob_params)
+
+    keypoints = blob_detector.detect(p_loc_bw_img)
+
+    points = np.asarray([kp.pt for kp in keypoints])
+
+    x_values = points[:, 0]
+    y_values = points[:, 1]
+
+    left_x = min(x_values)
+    right_x = max(x_values)
+    print('left_x=%.2lf, right_x=%.2lf' % (left_x, right_x))
+    mid_x = left_x + (right_x - left_x) / 2.0
+
+    min_dist = -1
+    mid_key_index = 0
+
+    for i, kp in enumerate(keypoints):
+        x = kp.pt[0]
+        dist = abs(x - mid_x)
+        if dist < min_dist or min_dist < 0:
+            min_dist = dist
+            mid_key_index = i
+
+    focus_x = int(x_values[mid_key_index])
+    focus_y = int(y_values[mid_key_index])
+    half_focus_area_width = 300 / 2
+    half_focus_area_height = 500 / 2
+
+    print(focus_x, focus_y)
+
+    p_loc_img_color = cv.cvtColor(p_loc_bw_img, cv.COLOR_GRAY2BGR)
+    focus_img = cv.rectangle(
+        p_loc_img_color,
+        (focus_x - half_focus_area_width, focus_y + half_focus_area_width),
+        (focus_x + half_focus_area_width, focus_y - half_focus_area_height),
+        (0, 255, 0),
+        thickness=6)
+
+    show_images([
+        ('focus_rec', focus_img),
+        ('p_loc_bw', p_loc_bw_img),
+        ('p_loc', p_loc_img),
+        ('original', img),
+        ('hue_img', hue_img),
+        ('lower_hue_range', lower_hue_range),
+        ('lower_mask', p_loc_mask),
+        ('grey', grey_img),
+    ], w, h)
+
+    cv.destroyAllWindows()
+
 def onMouseClicked(event, x, y, flags, img_param):
     if event == cv.EVENT_LBUTTONUP:
         print(img_param[y, x])
@@ -44,71 +114,4 @@ def show_images(images, w, h):
             index -= 1
 
 
-img = cv.imread(img_path, cv.IMREAD_COLOR)
-h, w = img.shape[:2]
-
-# get masks for planting locations
-hue_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-lower_hue_range = cv.inRange(hue_img, np.array([10, 40, 140]), np.array([30, 70, 210]))
-p_loc_mask = cv.addWeighted(lower_hue_range, 1.0, 1, 1.0, 0.0)
-
-# Apply mask to black + white image
-grey_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-p_loc_img = cv.bitwise_and(p_loc_mask, grey_img)
-thresh, p_loc_bw_img = cv.threshold(p_loc_img, 130, 255, cv.THRESH_BINARY)
-
-blobDetectorParams = cv.SimpleBlobDetector_Params()
-blobDetectorParams.blobColor = 255
-blobDetectorParams.filterByConvexity = False
-
-blobDetector = cv.SimpleBlobDetector_create(blobDetectorParams)
-
-keypoints = blobDetector.detect(p_loc_bw_img)
-
-points = np.asarray([kp.pt for kp in keypoints])
-
-x_values = points[:, 0]
-y_values = points[:, 1]
-
-left_x = min(x_values)
-right_x = max(x_values)
-mid_x = left_x + (right_x - left_x) / 2.0
-
-min_dist = -1
-mid_key_index = 0
-
-for i, kp in enumerate(keypoints):
-    x = kp.pt[0]
-    dist = abs(x - mid_x)
-    if dist < min_dist or min_dist < 0:
-        min_dist = dist
-        mid_key_index = i
-
-
-focus_x = int(x_values[mid_key_index])
-focus_y = int(y_values[mid_key_index])
-half_focus_area_width = 300 / 2
-half_focus_area_height = 500 / 2
-
-print(focus_x, focus_y)
-
-p_loc_img_color = cv.cvtColor(p_loc_img, cv.COLOR_GRAY2BGR)
-focus_img = cv.rectangle(
-    p_loc_img_color,
-    (focus_x - half_focus_area_width, focus_y + half_focus_area_width),
-    (focus_x + half_focus_area_width, focus_y - half_focus_area_height),
-    (0, 255, 0),
-    thickness=6)
-
-show_images([
-    ('focus_rec', focus_img),
-    ('p_loc_bw', p_loc_bw_img),
-    ('p_loc', p_loc_img),
-    ('original', img),
-    ('hue_img', hue_img),
-    ('lower_hue_range', lower_hue_range),
-    ('lower_mask', p_loc_mask),
-    ('grey', grey_img),
-], w, h)
-
-cv.destroyAllWindows()
+run()
